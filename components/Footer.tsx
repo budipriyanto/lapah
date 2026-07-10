8f7b18351a4a6b8db8b55bb5a4e6f107feb5f2e1
budipriyanto <boedipriyanto@gmail.com>
2026-07-10 17:44:38 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="border-t border-zinc-100 py-6 text-center text-sm text-[#737373]">
-      <p>&copy; {new Date().getFullYear()} Lapah. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Diskominfo - Lampung Timur. All rights reserved.</p>
     </footer>
   );
 }
